abad0e720c19006dedf8d127dec1c9a1308dd006
Faez-B <faezbhatti@gmail.com>
2023-03-24 16:51:10 +0100

    Article : route show user

--- app/src/Controller/ArticleController.php
+++ app/src/Controller/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Articles as Article;
 use App\Repository\ArticlesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,6 +17,14 @@ class ArticleController extends AbstractController
 
         return $this->render('article/index.html.twig', [
             'articles' => $articles,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_article_show')]
+    public function show(Article $article): Response
+    {
+        return $this->render('article/show.html.twig', [
+            'article' => $article,
         ]);
     }
 }
